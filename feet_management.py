234074fbebbea884da7a93d2e08276542a472ef7
Rocky Haque <99040331+rockyhaque@users.noreply.github.com>
2023-06-16 05:42:39 -0700

    Add files via upload

---
+++ feet_management.py
@@ -0,0 +1,34 @@
+# Incomplete Project
+# Ena Poribahon
+
+class Company:
+    def __init__(self, name, address, ) -> None:
+        self.name = name
+        self.bus = []
+        self.routes = []
+        self.drivers = []
+        self.counter = []
+        self.manager = []
+        self.supervisor = []
+        self.fare = []
+        pass
+
+class Driver:
+    def __init__(self, name, lincense, age) -> None:
+        self.license = license
+        self.name = name
+        self.age = age
+
+class Counter:
+    def __init__(self) -> None:
+        pass
+    def purchase_a_ticket(self, start, destination):
+        pass
+
+class Passenger:
+    pass
+
+class Supervisor:
+    pass
+
+manik_bhai = Driver('manik', '123', 32)
