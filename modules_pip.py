14cbe46eba859a464fe25228bdd0971b936fdd1b
Rocky <rockyhaque99@gmai.com>
2023-06-26 07:39:06 +0600

    variables

---
+++ modules_pip.py
@@ -0,0 +1,78 @@
+#Topic:
+""" 
+1. Module
+2. Escape
+3. Variable and Data types 
+
+"""
+
+
+# Moduls
+""" 
+import pandas
+import hashlib
+
+ """
+
+
+# Escape
+""" 
+print("Hello\npyth\'on!!!")
+
+# \ escape character
+
+print("Hey", 7, 'he\'s')
+print("Hey", 7, sep="~",  end="009\n") 
+# Hey~7 
+# Default separator is space( )
+print("Rocky")
+# print(object(s), sep=separator, end=end, file=file, flush=flush)
+ 
+"""
+
+
+
+
+
+
+# Variable and Data types
+#variable is like a container
+""" 
+a = 22
+print(a)
+b = "Rocky"
+print(b)
+c = complex(3, 5)
+print(c) 
+
+# print(a + b)
+# Not gonna work
+
+"""
+
+
+
+# 1. Sequenced data: list, tuple
+
+# list: A list is an ordered collection of data with elements separated by a comma and enclosed within square brackets. Lists are mutable and can be modified after creation.
+
+# list1 = [8, 2.3, [-4, 5], ['apple', 'banana']]
+# print(list1)
+
+
+
+# Tuple: A tuple is an ordered collection of data with elements separated by a comma and enclosed within parentheses. Tuples are immutable(Not changable) and can not be modified after creation.
+
+# tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+# print(tuple1)
+
+
+
+
+
+# 2. Mapped data: dict
+
+# dict: A dictionary is an unordered collection of data containing a key:value pair. The key:value pairs are enclosed within curly brackets.
+
+# dict1 = {"name":"Sakshi", "age":20, "canVote":True}
+# print(dict1)
