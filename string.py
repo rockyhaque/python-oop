185f461e5cefdc4c37291250fc877f8d258f17e2
Rocky Haque <rockyhaque99@gmail.com>
2023-06-26 12:11:10 +0600

    Create string.py

---
+++ string.py
@@ -0,0 +1,24 @@
+name = "Rocky"
+friend = "Ali"
+anotherFriend = "Huraira"
+
+juice = '''I will 
+complete my task
+by tomorrow '''
+
+""" 
+print("Hello, " + name)
+print(juice)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[5])
+print(name[6]) #Throw an error
+
+""" 
+
+
+print("Let\'s use a for loop,")
+for char in juice:
+    print(char)
