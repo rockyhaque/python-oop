98402e303160759bbcdacf17357cc9d90dd20545
Rocky Haque <rockyhaque99@gmail.com>
2023-06-26 19:38:27 +0600

    Add files via upload

---
+++ while_loop.py
@@ -0,0 +1,27 @@
+""" 
+i = 0
+while(i<3):
+    print(i)
+    i = i + 1 
+"""
+
+
+
+
+""" 
+i = int(input("Enter the value of i: "))
+while(i <= 20):
+    i = int(input("Enter the number: "))
+    # Take input from user untill  i <= 20
+    print(i)
+
+print("Done with the loop") 
+"""
+
+
+count = 4
+while(count>0):
+    print(count)
+    count = count - 1
+else:
+    print("I\'m inside the else")
