2a6497d81f6bce000f3682564ea50f32928966d2
Rocky Haque <rockyhaque99@gmail.com>
2023-06-28 08:01:44 +0600

    Add files via upload

---
+++ tuple_manipulation.py
@@ -0,0 +1,23 @@
+""" 
+-> Manipulating Tuples:
+
+Tuples are immutable, hence if you want to add, remove or change tuple items, then first you must convert the tuple to a list. Then perform operation on that list and convert it back to tuple.
+
+
+"""
+
+
+
+""" 
+
+countries = ("Spain", "Italy", "India", "England", "Germany")
+temp = list(countries)
+temp.append("Russia")       #add item 
+temp.pop(3)                 #remove item
+temp[2] = "Finland"         #change item
+countries = tuple(temp)
+print(countries) 
+
+"""
+
+
