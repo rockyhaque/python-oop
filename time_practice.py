fcc6f981c95499fe79c40b4b0948283d12d276fb
Rocky Haque <rockyhaque99@gmail.com>
2023-06-28 08:43:52 +0600

    Update time_practice.py

--- time_practice.py
+++ time_practice.py
@@ -1,10 +1,15 @@
+#This project will show to see your local time
+# Referance: https://docs.python.org/3/library/time.html#time.strftime
+
 import time
-timestamp = time.strftime('%H:%M:%S')
-print(timestamp)
-timestamp = time.strftime('%H')
-print(timestamp)
-timestamp = time.strftime('%M')
-print(timestamp)
-timestamp = time.strftime('%S')
-print(timestamp)
-# https://docs.python.org/3/library/time.html#time.strftime
+t = time.strftime('%H:%M:%S')
+hour = int(time.strftime('%H'))
+hour = int(input("Enter hour: "))
+print(hour)
+
+if(hour>=0 and hour<12):
+    print("Good Morning!")
+elif(hour>=12 and hour<17):
+    print("Good Afternoon!")
+elif(hour>=17 and hour<24):
+    print("Good Night!")
