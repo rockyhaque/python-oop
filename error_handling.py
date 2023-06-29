bedb1c33731b77d9ba16062e2bc009bf4b4c9ded
Rocky <rockyhaque99@gmai.com>
2023-06-29 20:35:07 +0600

    pushing again error

--- error_handling.py
+++ error_handling.py
@@ -1,6 +1,19 @@
+""" 
+
 try:
     num = int(input("Enter an integer: "))
 except ValueError:
     print("Number entered is not an integer.")
 
-# print('Recheck')
+# print('Recheck') 
+
+"""
+
+try:
+    num = int(input("Enter an integer: "))
+except ValueError:
+    print("Number entered is not an integer.")
+else:
+    print("Integer Accepted.")
+finally:
+    print("This block is always executed.")
