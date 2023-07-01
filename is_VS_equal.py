ab2b89ae9bb7c88e1208729e9ad7a651ff3a4540
Rocky Haque <rockyhaque99@gmail.com>
2023-07-01 18:36:15 +0600

    Add files via upload

---
+++ is_VS_equal.py
@@ -0,0 +1,21 @@
+a = [1, 2, 3]
+b = [1, 2, 3]
+
+print(a == b)  # exact location of object in memory
+print(a is b)  # value
+
+
+
+a = "hello"
+b = "hello"
+
+print(a == b)  # True
+print(a is b)  # True
+
+
+
+a = 5
+b = 5
+
+print(a == b)  # True
+print(a is b)  # True
